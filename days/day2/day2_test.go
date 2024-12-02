7a7c1bba0be55a3273dbc17eccf9232d33096392
tomasff <15079464+tomasff@users.noreply.github.com>
2024-12-02 18:01:24 +0000

    Day 2: Add tests

--- days/day2/day2_test.go
+++ days/day2/day2_test.go
@@ -1,1 +1,39 @@
 package day2
+
+import "testing"
+
+var reports = [][]int{
+	{7, 6, 4, 2, 1},
+	{1, 2, 7, 8, 9},
+	{9, 7, 6, 2, 1},
+	{1, 3, 2, 4, 5},
+	{8, 6, 4, 4, 1},
+	{1, 3, 6, 7, 9},
+}
+
+const numStrictSafeReports = 2
+const numDampenedSafeReports = 4
+
+func TestCountSafeReports(t *testing.T) {
+	tentativeSolution := countSafeReports(reports)
+
+	if tentativeSolution != numStrictSafeReports {
+		t.Fatalf(
+			`Expected %d strictly safe reports, counted %d.`,
+			numStrictSafeReports,
+			tentativeSolution,
+		)
+	}
+}
+
+func TestCountDampenedSafeReports(t *testing.T) {
+	tentativeSolution := countSafeReportsWithDampener(reports)
+
+	if tentativeSolution != numDampenedSafeReports {
+		t.Fatalf(
+			`Expected %d dampened safe reports, counted %d.`,
+			numDampenedSafeReports,
+			tentativeSolution,
+		)
+	}
+}
